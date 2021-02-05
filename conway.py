de9db10a65bc5fd02bbe09bf836392b09c96d998
Mariana Avalos <mariana.avalos.arce@gmail.com>
2021-02-05 08:41:44 -0600

    glider moves
    Signed-off-by: Mariana Avalos <mariana.avalos.arce@gmail.com>

--- conway.py
+++ conway.py
@@ -23,13 +23,41 @@ def addGlider(i, j, grid):
                        [0,  255, 255]])
     grid[i:i+3, j:j+3] = glider
 
-def update(frameNum, img, grid, N):
+def checkCell(r, c, grid):
+    #alive = []
+    alive = 0
+    for i in range(r - 1, r + 2, 1):
+        for j in range(c - 1, c + 2, 1):
+            if i == r and j == c:
+                continue
+            if i >= len(grid) or j >= len(grid) or i < 0 or j < 0:
+                continue
+            if grid[i][j] != 0:
+                #alive.append(tuple([i, j]))
+                alive += 1
+    return alive
+
+
+def update(frameNum, img, grid, N, ax):
     # copy grid since we require 8 neighbors for calculation
     # and we go line by line 
     newGrid = grid.copy()
     # TODO: Implement the rules of Conway's Game of Life
 
+    for i in range(N):
+        for j in range(N):
+            myNeighbours = checkCell(i, j, grid)
+            me = grid[i][j]
+            if me != 0 and myNeighbours < 2: # underpopulation
+                newGrid[i][j] = 0
+            if me != 0 and (myNeighbours == 2 or myNeighbours == 3): # next generation
+                newGrid[i][j] = 255
+            if me != 0 and myNeighbours > 3: # overpopulation
+                newGrid[i][j] = 0
+            if me != 255 and myNeighbours == 3: # reproduction
+                newGrid[i][j] = 255
     # update data
+    ax.set_title("Frame = {0}".format(frameNum))
     img.set_data(newGrid)
     grid[:] = newGrid[:]
     return img,
@@ -43,23 +71,23 @@ def main():
     # TODO: add arguments
     
     # set grid size
-    N = 100
+    N = 20
         
     # set animation update interval
-    updateInterval = 50
+    updateInterval = 500
 
     # declare grid
     grid = np.array([])
     # populate grid with random on/off - more off than on
     grid = randomGrid(N)
     # Uncomment lines to see the "glider" demo
-    #grid = np.zeros(N*N).reshape(N, N)
-    #addGlider(1, 1, grid)
+    grid = np.zeros(N*N).reshape(N, N)
+    addGlider(int(N/2), int(N/2), grid)
 
     # set up animation
     fig, ax = plt.subplots()
     img = ax.imshow(grid, interpolation='nearest')
-    ani = animation.FuncAnimation(fig, update, fargs=(img, grid, N, ),
+    ani = animation.FuncAnimation(fig, update, fargs=(img, grid, N, ax, ),
                                   frames = 10,
                                   interval=updateInterval,
                                   save_count=50)
