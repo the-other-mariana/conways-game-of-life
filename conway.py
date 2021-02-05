765c4bc7266869d2b56861227b42456bcb5b471a
Mariana Avalos <mariana.avalos.arce@gmail.com>
2021-02-05 07:19:03 -0600

    base code
    Signed-off-by: Mariana Avalos <mariana.avalos.arce@gmail.com>

---
+++ conway.py
@@ -0,0 +1,71 @@
+"""
+conway.py 
+A simple Python/matplotlib implementation of Conway's Game of Life.
+"""
+
+import sys, argparse
+import numpy as np
+import matplotlib.pyplot as plt 
+import matplotlib.animation as animation
+
+ON = 255
+OFF = 0
+vals = [ON, OFF]
+
+def randomGrid(N):
+    """returns a grid of NxN random values"""
+    return np.random.choice(vals, N*N, p=[0.2, 0.8]).reshape(N, N)
+
+def addGlider(i, j, grid):
+    """adds a glider with top left cell at (i, j)"""
+    glider = np.array([[0,    0, 255], 
+                       [255,  0, 255], 
+                       [0,  255, 255]])
+    grid[i:i+3, j:j+3] = glider
+
+def update(frameNum, img, grid, N):
+    # copy grid since we require 8 neighbors for calculation
+    # and we go line by line 
+    newGrid = grid.copy()
+    # TODO: Implement the rules of Conway's Game of Life
+
+    # update data
+    img.set_data(newGrid)
+    grid[:] = newGrid[:]
+    return img,
+
+# main() function
+def main():
+    # Command line args are in sys.argv[1], sys.argv[2] ..
+    # sys.argv[0] is the script name itself and can be ignored
+    # parse arguments
+    parser = argparse.ArgumentParser(description="Runs Conway's Game of Life system.py.")
+    # TODO: add arguments
+    
+    # set grid size
+    N = 100
+        
+    # set animation update interval
+    updateInterval = 50
+
+    # declare grid
+    grid = np.array([])
+    # populate grid with random on/off - more off than on
+    grid = randomGrid(N)
+    # Uncomment lines to see the "glider" demo
+    #grid = np.zeros(N*N).reshape(N, N)
+    #addGlider(1, 1, grid)
+
+    # set up animation
+    fig, ax = plt.subplots()
+    img = ax.imshow(grid, interpolation='nearest')
+    ani = animation.FuncAnimation(fig, update, fargs=(img, grid, N, ),
+                                  frames = 10,
+                                  interval=updateInterval,
+                                  save_count=50)
+
+    plt.show()
+
+# call main
+if __name__ == '__main__':
+    main()
